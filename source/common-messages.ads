47800eda6e93d31a8accc8132f073106d1e9a4e1
Jerrid Kimball <jkimball@nairobi.home.ada.cx>
2012-10-20 01:43:58 -0500

    Initial commit.

---
+++ source/common-messages.ads
@@ -0,0 +1,12 @@
+
+package Common.Messages is
+   No_URL            : constant String := "No URL specified";
+   No_Protocol       : constant String := "URL must specify protocol";
+   Invalid_URL       : constant String := "URL looks invalid";
+   Bad_Link          : constant String := "Host says this link is bad";
+   Dead_Link         : constant String := "Link seems dead";
+   Invalid_Alias     : constant String := "Invalid Alias";
+   Not_Found_Alias   : constant String := "Alias not found";
+   Database_Failure  : constant String := "Database failed";
+   Unknown_Exception : constant String := "Unknown Exception";
+end Common.Messages;
---
+++ source/common.ads
@@ -0,0 +1,4 @@
+
+package Common is
+
+end Common;
